5f149ed09d5611e000ec2101ef8d3d3f6e2cf9da
Kévin M <neocorp_fr@hotmail.com>
2023-06-23 15:12:04 +0200

    Full clean
    Remove Bootstrap, Jquery…

---
+++ assets/js/carousel.js
@@ -0,0 +1,25 @@
+const carourelItems = document.querySelectorAll('.carousel-item');
+let carouselIndex = 0;
+console.log(carourelItems[0]);
+
+function carouselChange(a,b){
+    carouselIndex += a;
+    if (carouselIndex >= carourelItems.length) {
+        carouselIndex = 0;
+    } else if (carouselIndex < 0) {
+        carouselIndex = carourelItems.length - 1;
+    }
+    
+    if(b){
+        carouselIndex = b - 1;
+    }
+    
+    for (var ci = 0; ci < carourelItems.length; ci++) {
+        if(ci == carouselIndex){
+            carourelItems[ci].classList.add("active");
+        }else{
+            carourelItems[ci].classList.remove("active");
+        }
+    }
+
+}
---
+++ assets/js/gallerie.js
@@ -0,0 +1,65 @@
+
+const modal = document.getElementById("modal");
+const modalImage = document.getElementById("modal-image");
+const galleryItems = document.getElementsByClassName("gallery-item");
+const filters = document.querySelectorAll(".gallery-filter li");
+
+
+let currentIndex = 0;
+// Afficher la modale avec l'image sélectionnée
+function openModal(index) {
+    currentIndex = index;
+    modal.style.display = "block";
+    modalImage.src = galleryItems[index].src;
+}
+
+// Fermer la modale
+function closeModal() {
+    modal.style.display = "none";
+}
+
+// Changer l'image affichée
+function changeImage(n) {
+    currentIndex += n;
+    if (currentIndex >= galleryItems.length) {
+        currentIndex = 0;
+    } else if (currentIndex < 0) {
+        currentIndex = galleryItems.length - 1;
+    }
+    modalImage.src = galleryItems[currentIndex].src;
+}
+
+// Ajouter des événements de clic aux images de la galerie
+for (var i = 0; i < galleryItems.length; i++) {
+    galleryItems[i].addEventListener("click", function (index) {
+        return function () {
+            openModal(index);
+        };
+    }(i));
+}
+
+function filterItems(nameFilter){
+    for (var i = 0; i < galleryItems.length; i++) {
+        if(nameFilter === 'Tous'){
+            galleryItems[i].classList.remove("hide");
+        }else{
+            if(galleryItems[i].dataset.galleryTag != nameFilter){
+                galleryItems[i].classList.add("hide");
+            }else{
+                galleryItems[i].classList.remove("hide");
+            }
+        }
+    }
+    for(var i = 0; i < filters.length; i++){
+        if(filters[i].innerHTML == nameFilter){
+            filters[i].classList.add("gallery-filter-active");
+        }else{
+            filters[i].classList.remove("gallery-filter-active");
+        }
+    }
+
+}
+
+
+// Ajouter un événement de clic pour fermer la modale
+//modal.addEventListener("click", closeModal);
